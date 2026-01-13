42cc168ef57e6882c043bb9f3fad87af4974fe64
liza.beth <anelizamr@gmail.com>
2026-01-13 10:25:39 -0300

    primeiro exercício

---
+++ exercicios1.py
@@ -0,0 +1,30 @@
+# Crie um programa que imprima a mensagem "Olá, mundo!" na tela.
+print("Olá, mundo")
+
+# Desenvolva um programa que soma dois números. Use as variáveis numero1 = 12 e numero2 = 14. O programa deve calcular a soma e exibir o resultado.
+numero1 = 12
+numero2 = 14
+numero3 = numero1 + numero2
+print(numero3)
+
+# Crie um programa que calcula o volume de uma caixa retangular. Use as seguintes dimensões:
+# * Comprimento: 12 cm
+# * Largura: 14 cm
+# * Altura: 20 cm
+# O programa deve calcular o volume e exibir o resultado em cm³.
+comprimento = 12
+largura = 14
+altura = 20
+print(f'{comprimento*largura*altura} cm3')
+
+# Calculadora de Preço Total
+# * Desenvolva um programa que calcula o preço total de uma compra. Use as seguintes informações:
+# * Nome do produto: "Cadeira Infantil"
+# * Preço unitário: R$ 12.40
+# * Quantidade: 3
+# O programa deve calcular o preço total e exibir todas as informações, incluindo o resultado final.
+nomeProduto = "Cadeira Infantil"
+precoUnitario = 12.40
+quantidade = 3
+precoTotal = precoUnitario*quantidade
+print(f'O Produto {nomeProduto} custa {precoUnitario}. Ao comprar {quantidade}, o preço total é {precoTotal}')
